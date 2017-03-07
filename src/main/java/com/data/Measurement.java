9e3421ee79a1fd7fe97981bc072f55362608f433
Prashant Shandilya <prashant.shandilya@capgemini.com>
2017-03-07 17:14:59 +0530

    Added more fields

--- src/main/java/com/data/Measurement.java
+++ src/main/java/com/data/Measurement.java
@@ -2,21 +2,15 @@ package com.data;
 
 public class Measurement {
 
-	private Double front_neck_to_mid_neck;
+	private Double back_neck_to_waist;
 
-	private Double high_hip_height;
+	private Double back_arm_length;
 
-	private Double armscye_girth;
+	private Double bust_girth;
 
-	private Double weight;
+	private Double shoulder_length;
 
-	private Double under_bust_height;
-
-	private Double mid_neck_girth;
-
-	private Double front_neck_to_waist;
-
-	private Double side_arm_length;
+	private Double ankle_girth;
 
 	private Double waist_to_high_hip;
 
@@ -24,94 +18,128 @@ public class Measurement {
 
 	private Double height;
 
-	private Double thigh_height;
+	private Double side_neck_to_bust_point;
+
+	private Double back_neck_to_across_back;
 
 	private Double waist_to_low_hip;
 
-	private Double across_front_width;
+	private Double above_bust_girth;
 
-	private Double total_rise_length;
+	private Double across_back_width;
+
+	private Double neck_base_girth;
 
 	private Double low_hip_girth;
 
-	private Double bust_height;
+	private Double full_sleeve_length;
+
+	private Double armscye_girth;
+
+	private Double shoulder_drop;
+
+	private Double elbow_girth;
+
+	private Double calf_height;
+
+	private Double wrist_girth;
+
+	private Double across_back_shoulder_width;
+
+	private Double bicep_girth;
+
+	private Double thigh_height;
+
+	private Double shirt_sleeve_length;
 
 	private Double knee_height;
 
-	private Double bust_girth;
+	private Double ankle_height;
+
+	private Double back_neck_height;
+
+	private Double high_hip_height;
+
+	private Double inseam;
+
+	private Double weight;
 
 	private Double waist_girth;
 
-	public Double getFront_neck_to_mid_neck() {
-		return front_neck_to_mid_neck;
+	private Double waist_height;
+
+	private Double front_neck_to_waist;
+
+	private Double thigh_girth;
+
+	private Double calf_girth;
+
+	private Double outseam;
+
+	private Double across_front_width;
+
+	private Double knee_girth;
+
+	private Double total_rise_length;
+
+	private Double back_neck_to_mid_neck;
+
+	private Double front_neck_to_mid_neck;
+
+	private Double vertical_trunk_girth;
+
+	private Double mid_neck_girth;
+
+	private Double front_neck_height;
+
+	private Double side_arm_length;
+
+	private Double forearm_girth;
+
+	private Double high_hip_girth;
+
+	private Double low_hip_height;
+
+	private Double bust_height;
+
+	public Double getBack_neck_to_waist() {
+		return back_neck_to_waist;
 	}
 
-	public void setFront_neck_to_mid_neck(Double front_neck_to_mid_neck) {
-		this.front_neck_to_mid_neck = front_neck_to_mid_neck;
+	public void setBack_neck_to_waist(Double back_neck_to_waist) {
+		this.back_neck_to_waist = back_neck_to_waist;
 	}
 
-	public Double getHigh_hip_height() {
-		return high_hip_height;
+	public Double getBack_arm_length() {
+		return back_arm_length;
 	}
 
-	public void setHigh_hip_height(Double high_hip_height) {
-		this.high_hip_height = high_hip_height;
+	public void setBack_arm_length(Double back_arm_length) {
+		this.back_arm_length = back_arm_length;
 	}
 
-	public Double getArmscye_girth() {
-		return armscye_girth;
+	public Double getBust_girth() {
+		return bust_girth;
 	}
 
-	public void setArmscye_girth(Double armscye_girth) {
-		this.armscye_girth = armscye_girth;
+	public void setBust_girth(Double bust_girth) {
+		this.bust_girth = bust_girth;
 	}
 
-	public Double getWeight() {
-		return weight;
+	public Double getShoulder_length() {
+		return shoulder_length;
 	}
 
-	public void setWeight(Double weight) {
-		this.weight = weight;
+	public void setShoulder_length(Double shoulder_length) {
+		this.shoulder_length = shoulder_length;
 	}
 
-	public Double getUnder_bust_height() {
-		return under_bust_height;
+	public Double getAnkle_girth() {
+		return ankle_girth;
 	}
 
-	public void setUnder_bust_height(Double under_bust_height) {
-		this.under_bust_height = under_bust_height;
-	}
-
-	public Double getWaist_girth() {
-		return waist_girth;
-	}
-
-	public void setWaist_girth(Double waist_girth) {
-		this.waist_girth = waist_girth;
-	}
-
-	public Double getMid_neck_girth() {
-		return mid_neck_girth;
-	}
-
-	public void setMid_neck_girth(Double mid_neck_girth) {
-		this.mid_neck_girth = mid_neck_girth;
-	}
-
-	public Double getFront_neck_to_waist() {
-		return front_neck_to_waist;
-	}
-
-	public void setFront_neck_to_waist(Double front_neck_to_waist) {
-		this.front_neck_to_waist = front_neck_to_waist;
-	}
-
-	public Double getSide_arm_length() {
-		return side_arm_length;
-	}
-
-	public void setSide_arm_length(Double side_arm_length) {
-		this.side_arm_length = side_arm_length;
+	public void setAnkle_girth(Double ankle_girth) {
+		this.ankle_girth = ankle_girth;
 	}
 
 	public Double getWaist_to_high_hip() {
@@ -138,12 +166,20 @@ public class Measurement {
 		this.height = height;
 	}
 
-	public Double getThigh_height() {
-		return thigh_height;
+	public Double getSide_neck_to_bust_point() {
+		return side_neck_to_bust_point;
 	}
 
-	public void setThigh_height(Double thigh_height) {
-		this.thigh_height = thigh_height;
+	public void setSide_neck_to_bust_point(Double side_neck_to_bust_point) {
+		this.side_neck_to_bust_point = side_neck_to_bust_point;
+	}
+
+	public Double getBack_neck_to_across_back() {
+		return back_neck_to_across_back;
+	}
+
+	public void setBack_neck_to_across_back(Double back_neck_to_across_back) {
+		this.back_neck_to_across_back = back_neck_to_across_back;
 	}
 
 	public Double getWaist_to_low_hip() {
@@ -154,20 +190,28 @@ public class Measurement {
 		this.waist_to_low_hip = waist_to_low_hip;
 	}
 
-	public Double getAcross_front_width() {
-		return across_front_width;
+	public Double getAbove_bust_girth() {
+		return above_bust_girth;
 	}
 
-	public void setAcross_front_width(Double across_front_width) {
-		this.across_front_width = across_front_width;
+	public void setAbove_bust_girth(Double above_bust_girth) {
+		this.above_bust_girth = above_bust_girth;
 	}
 
-	public Double getTotal_rise_length() {
-		return total_rise_length;
+	public Double getAcross_back_width() {
+		return across_back_width;
 	}
 
-	public void setTotal_rise_length(Double total_rise_length) {
-		this.total_rise_length = total_rise_length;
+	public void setAcross_back_width(Double across_back_width) {
+		this.across_back_width = across_back_width;
+	}
+
+	public Double getNeck_base_girth() {
+		return neck_base_girth;
+	}
+
+	public void setNeck_base_girth(Double neck_base_girth) {
+		this.neck_base_girth = neck_base_girth;
 	}
 
 	public Double getLow_hip_girth() {
@@ -178,12 +222,84 @@ public class Measurement {
 		this.low_hip_girth = low_hip_girth;
 	}
 
-	public Double getBust_height() {
-		return bust_height;
+	public Double getFull_sleeve_length() {
+		return full_sleeve_length;
 	}
 
-	public void setBust_height(Double bust_height) {
-		this.bust_height = bust_height;
+	public void setFull_sleeve_length(Double full_sleeve_length) {
+		this.full_sleeve_length = full_sleeve_length;
+	}
+
+	public Double getArmscye_girth() {
+		return armscye_girth;
+	}
+
+	public void setArmscye_girth(Double armscye_girth) {
+		this.armscye_girth = armscye_girth;
+	}
+
+	public Double getShoulder_drop() {
+		return shoulder_drop;
+	}
+
+	public void setShoulder_drop(Double shoulder_drop) {
+		this.shoulder_drop = shoulder_drop;
+	}
+
+	public Double getElbow_girth() {
+		return elbow_girth;
+	}
+
+	public void setElbow_girth(Double elbow_girth) {
+		this.elbow_girth = elbow_girth;
+	}
+
+	public Double getCalf_height() {
+		return calf_height;
+	}
+
+	public void setCalf_height(Double calf_height) {
+		this.calf_height = calf_height;
+	}
+
+	public Double getWrist_girth() {
+		return wrist_girth;
+	}
+
+	public void setWrist_girth(Double wrist_girth) {
+		this.wrist_girth = wrist_girth;
+	}
+
+	public Double getAcross_back_shoulder_width() {
+		return across_back_shoulder_width;
+	}
+
+	public void setAcross_back_shoulder_width(Double across_back_shoulder_width) {
+		this.across_back_shoulder_width = across_back_shoulder_width;
+	}
+
+	public Double getBicep_girth() {
+		return bicep_girth;
+	}
+
+	public void setBicep_girth(Double bicep_girth) {
+		this.bicep_girth = bicep_girth;
+	}
+
+	public Double getThigh_height() {
+		return thigh_height;
+	}
+
+	public void setThigh_height(Double thigh_height) {
+		this.thigh_height = thigh_height;
+	}
+
+	public Double getShirt_sleeve_length() {
+		return shirt_sleeve_length;
+	}
+
+	public void setShirt_sleeve_length(Double shirt_sleeve_length) {
+		this.shirt_sleeve_length = shirt_sleeve_length;
 	}
 
 	public Double getKnee_height() {
@@ -194,25 +310,222 @@ public class Measurement {
 		this.knee_height = knee_height;
 	}
 
-	public Double getBust_girth() {
-		return bust_girth;
+	public Double getAnkle_height() {
+		return ankle_height;
 	}
 
-	public void setBust_girth(Double bust_girth) {
-		this.bust_girth = bust_girth;
+	public void setAnkle_height(Double ankle_height) {
+		this.ankle_height = ankle_height;
+	}
+
+	public Double getBack_neck_height() {
+		return back_neck_height;
+	}
+
+	public void setBack_neck_height(Double back_neck_height) {
+		this.back_neck_height = back_neck_height;
+	}
+
+	public Double getHigh_hip_height() {
+		return high_hip_height;
+	}
+
+	public void setHigh_hip_height(Double high_hip_height) {
+		this.high_hip_height = high_hip_height;
+	}
+
+	public Double getInseam() {
+		return inseam;
+	}
+
+	public void setInseam(Double inseam) {
+		this.inseam = inseam;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Double getWaist_girth() {
+		return waist_girth;
+	}
+
+	public void setWaist_girth(Double waist_girth) {
+		this.waist_girth = waist_girth;
+	}
+
+	public Double getWaist_height() {
+		return waist_height;
+	}
+
+	public void setWaist_height(Double waist_height) {
+		this.waist_height = waist_height;
+	}
+
+	public Double getFront_neck_to_waist() {
+		return front_neck_to_waist;
+	}
+
+	public void setFront_neck_to_waist(Double front_neck_to_waist) {
+		this.front_neck_to_waist = front_neck_to_waist;
+	}
+
+	public Double getThigh_girth() {
+		return thigh_girth;
+	}
+
+	public void setThigh_girth(Double thigh_girth) {
+		this.thigh_girth = thigh_girth;
+	}
+
+	public Double getCalf_girth() {
+		return calf_girth;
+	}
+
+	public void setCalf_girth(Double calf_girth) {
+		this.calf_girth = calf_girth;
+	}
+
+	public Double getOutseam() {
+		return outseam;
+	}
+
+	public void setOutseam(Double outseam) {
+		this.outseam = outseam;
+	}
+
+	public Double getAcross_front_width() {
+		return across_front_width;
+	}
+
+	public void setAcross_front_width(Double across_front_width) {
+		this.across_front_width = across_front_width;
+	}
+
+	public Double getKnee_girth() {
+		return knee_girth;
+	}
+
+	public void setKnee_girth(Double knee_girth) {
+		this.knee_girth = knee_girth;
+	}
+
+	public Double getTotal_rise_length() {
+		return total_rise_length;
+	}
+
+	public void setTotal_rise_length(Double total_rise_length) {
+		this.total_rise_length = total_rise_length;
+	}
+
+	public Double getBack_neck_to_mid_neck() {
+		return back_neck_to_mid_neck;
+	}
+
+	public void setBack_neck_to_mid_neck(Double back_neck_to_mid_neck) {
+		this.back_neck_to_mid_neck = back_neck_to_mid_neck;
+	}
+
+	public Double getFront_neck_to_mid_neck() {
+		return front_neck_to_mid_neck;
+	}
+
+	public void setFront_neck_to_mid_neck(Double front_neck_to_mid_neck) {
+		this.front_neck_to_mid_neck = front_neck_to_mid_neck;
+	}
+
+	public Double getVertical_trunk_girth() {
+		return vertical_trunk_girth;
+	}
+
+	public void setVertical_trunk_girth(Double vertical_trunk_girth) {
+		this.vertical_trunk_girth = vertical_trunk_girth;
+	}
+
+	public Double getMid_neck_girth() {
+		return mid_neck_girth;
+	}
+
+	public void setMid_neck_girth(Double mid_neck_girth) {
+		this.mid_neck_girth = mid_neck_girth;
+	}
+
+	public Double getFront_neck_height() {
+		return front_neck_height;
+	}
+
+	public void setFront_neck_height(Double front_neck_height) {
+		this.front_neck_height = front_neck_height;
+	}
+
+	public Double getSide_arm_length() {
+		return side_arm_length;
+	}
+
+	public void setSide_arm_length(Double side_arm_length) {
+		this.side_arm_length = side_arm_length;
+	}
+
+	public Double getForearm_girth() {
+		return forearm_girth;
+	}
+
+	public void setForearm_girth(Double forearm_girth) {
+		this.forearm_girth = forearm_girth;
+	}
+
+	public Double getHigh_hip_girth() {
+		return high_hip_girth;
+	}
+
+	public void setHigh_hip_girth(Double high_hip_girth) {
+		this.high_hip_girth = high_hip_girth;
+	}
+
+	public Double getLow_hip_height() {
+		return low_hip_height;
+	}
+
+	public void setLow_hip_height(Double low_hip_height) {
+		this.low_hip_height = low_hip_height;
+	}
+
+	public Double getBust_height() {
+		return bust_height;
+	}
+
+	public void setBust_height(Double bust_height) {
+		this.bust_height = bust_height;
 	}
 
 	@Override
 	public String toString() {
-		return "ClassPojo [front_neck_to_mid_neck = " + front_neck_to_mid_neck + ", high_hip_height = "
-				+ high_hip_height + ", armscye_girth = " + armscye_girth + ", weight = " + weight
-				+ ", under_bust_height = " + under_bust_height + ", waist_girth = " + waist_girth
-				+ ", mid_neck_girth = " + mid_neck_girth + ", front_neck_to_waist = " + front_neck_to_waist
-				+ ", side_arm_length = " + side_arm_length + ", waist_to_high_hip = " + waist_to_high_hip
-				+ ", front_neck_to_across_front = " + front_neck_to_across_front + ", height = " + height
-				+ ", thigh_height = " + thigh_height + ", waist_to_low_hip = " + waist_to_low_hip
-				+ ", across_front_width = " + across_front_width + ", total_rise_length = " + total_rise_length
-				+ ", low_hip_girth = " + low_hip_girth + ", bust_height = " + bust_height + ", knee_height = "
-				+ knee_height + "]";
+		return "ClassPojo [back_neck_to_waist = " + back_neck_to_waist + ", back_arm_length = " + back_arm_length
+				+ ", bust_girth = " + bust_girth + ", shoulder_length = " + shoulder_length + ", ankle_girth = "
+				+ ankle_girth + ", waist_to_high_hip = " + waist_to_high_hip + ", front_neck_to_across_front = "
+				+ front_neck_to_across_front + ", height = " + height + ", side_neck_to_bust_point = "
+				+ side_neck_to_bust_point + ", back_neck_to_across_back = " + back_neck_to_across_back
+				+ ", waist_to_low_hip = " + waist_to_low_hip + ", above_bust_girth = " + above_bust_girth
+				+ ", across_back_width = " + across_back_width + ", neck_base_girth = " + neck_base_girth
+				+ ", low_hip_girth = " + low_hip_girth + ", full_sleeve_length = " + full_sleeve_length
+				+ ", armscye_girth = " + armscye_girth + ", shoulder_drop = " + shoulder_drop + ", elbow_girth = "
+				+ elbow_girth + ", calf_height = " + calf_height + ", wrist_girth = " + wrist_girth
+				+ ", across_back_shoulder_width = " + across_back_shoulder_width + ", bicep_girth = " + bicep_girth
+				+ ", thigh_height = " + thigh_height + ", shirt_sleeve_length = " + shirt_sleeve_length
+				+ ", knee_height = " + knee_height + ", ankle_height = " + ankle_height + ", back_neck_height = "
+				+ back_neck_height + ", high_hip_height = " + high_hip_height + ", inseam = " + inseam + ", weight = "
+				+ weight + ", waist_girth = " + waist_girth + ", waist_height = " + waist_height
+				+ ", front_neck_to_waist = " + front_neck_to_waist + ", thigh_girth = " + thigh_girth
+				+ ", calf_girth = " + calf_girth + ", outseam = " + outseam + ", across_front_width = "
+				+ across_front_width + ", knee_girth = " + knee_girth + ", total_rise_length = " + total_rise_length
+				+ ", back_neck_to_mid_neck = " + back_neck_to_mid_neck + ", front_neck_to_mid_neck = "
+				+ front_neck_to_mid_neck + ", vertical_trunk_girth = " + vertical_trunk_girth + ", mid_neck_girth = "
+				+ mid_neck_girth + ", front_neck_height = " + front_neck_height + ", side_arm_length = "
+				+ side_arm_length + ", forearm_girth = " + forearm_girth + ", high_hip_girth = " + high_hip_girth
+				+ ", low_hip_height = " + low_hip_height + ", bust_height = " + bust_height + "]";
 	}
 }
